3f4281cc71a9cc15674dfd52cb38359ec0306dba
준성 <madox9999@gmail.com>
2021-05-25 20:57:39 +0900

    상세 페이지 애니메이션 수정

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_jetsnack/ui/body_mid/card_mid.dart';
 
@@ -15,13 +16,14 @@ class _DetailPageState extends State<DetailPage> {
 
   void initState() {
     scrollController.addListener(() {
-      if (offset && scrollController.offset != 0) {
-        offset = false;
-        setState(() {});
-      } else if (!offset && scrollController.offset == 0) {
-        offset = true;
-        setState(() {});
-      }
+      print(scrollController.offset);
+      // if (offset && scrollController.offset != 0) {
+      //   offset = true;
+      //   setState(() {});
+      // } else if (!offset && scrollController.offset == 1) {
+      //   offset = false;
+      //   setState(() {});
+      // }
     });
     super.initState();
   }
@@ -30,12 +32,11 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
+        controller: scrollController,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ListView(
-              primary: false,
-              controller: scrollController,
               scrollDirection: Axis.vertical,
               shrinkWrap: true,
               children: [
@@ -47,13 +48,12 @@ class _DetailPageState extends State<DetailPage> {
                       height: 200,
                     ),
                     Positioned(
-                      top: 0,
                       child: FittedBox(
                         child: AnimatedPositioned(
-                          width: offset ? 70 : 200,
-                          duration: Duration(seconds: 1),
+                          duration: Duration(seconds: 2),
+                          curve: Curves.easeInOut,
                           child: Container(
-                            width: 200,
+                            width: offset ? 200 : 100,
                             height: 200,
                             decoration: BoxDecoration(
                               image: DecorationImage(
@@ -62,6 +62,7 @@ class _DetailPageState extends State<DetailPage> {
                               ),
                               color: Colors.black,
                               shape: BoxShape.circle,
+
                             ),
                           ),
                         ),
@@ -70,26 +71,38 @@ class _DetailPageState extends State<DetailPage> {
                   ],
                 ),
                 Divider(),
-                Text('Cupcake',
-                    style:
-                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                Text(
-                  'A tag line',
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Cupcake',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
                 ),
-                Text(
-                  '\$12,99',
-                  style: TextStyle(
-                      color: Colors.deepPurple,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    'A tag line',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '\$12,99',
+                    style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
                 Divider(),
-                Text(
-                    'Details \n Lorem ipsum dolor sit amet, consectetur \n adipiscing elit, sed do eiusmod tempor \n incididunt ut labore et dolore magna aliqua Ut \n enim ad minim veniam, quis nostrud..'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                      ' Details \n Lorem ipsum dolor sit amet, consectetur \n adipiscing elit, sed do eiusmod tempor \n incididunt ut labore et dolore magna aliqua Ut \n enim ad minim veniam, quis nostrud..'),
+                ),
                 Divider(),
               ],
             ),
